52019d6d523eae8ceb22eaf65398c71707aead27
Skye Shaxx <skydriveshax@hotmail.co.uk>
2016-11-22 22:29:02 +0000

    Added Singleton Debug Logger (Saves Log To File)

---
+++ ConsoleApplication1/Debugging/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1.Debug {
+    public class Logger {
+        private static Logger _instance;
+        private StreamWriter _streamWriter;
+
+        private Logger() { }
+
+        public static Logger getInstance() {
+            if (null == _instance) {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+
+        public void WriteLine(string log) {
+            _streamWriter = new StreamWriter("log.txt", true);
+            _streamWriter.WriteLine(DateTime.Now.ToString() + ": " + log);
+            _streamWriter.Close();
+            _streamWriter.Dispose();
+        }
+    }
+}
