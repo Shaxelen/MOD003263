b7edc5f55781aae565650e14877dbc64b4b7b22c
296439 <296439@H0829>
2016-11-28 15:28:38 +0000

    Started to Unit Test Person Classes

---
+++ UITests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MOD003263_SoftwareEngineering.Forms;
+using MOD003263_SoftwareEngineering.UI;
+using MOD003263_SoftwareEngineering.Core;
+
+namespace UITests {
+    [TestClass]
+    public class UnitTest {
+        [TestMethod]
+        public void PersonTest() {
+            Applicant a = new Applicant();
+
+            a.ApplicantID = 111;
+            a.FirstName = "Alex";
+            a.LastName = "Wyke";
+            a.EmailAddress = "alexwyke@icloud.com";
+            a.PhoneNumber = "07456679770";
+            a.ApplicantPosition = "Website Developer";
+        }
+    }
+}
