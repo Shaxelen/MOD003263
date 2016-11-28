3645108974f10391815222802886d8577f2263b4
351753 <351753@H0818>
2016-11-28 15:57:40 +0000

    Added Template UnitTests for Meta Layer <Still needs doing>

---
+++ MetaUnitTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MOD003263_SoftwareEngineering.Meta;
+using MOD003263_SoftwareEngineering.Core;
+
+namespace MetaUnitTests {
+    [TestClass]
+    public class MetaUnitTest {
+        [TestMethod]
+        public void TemplateSerializationTest() {
+
+        }
+        [TestMethod]
+        public void PDFConvertTest() {
+
+        }
+        [TestMethod]
+        public void OleDbConnectionTest() {
+
+        }
+        [TestMethod]
+        public void DatabaseMetaLayerTest() {
+
+        }
+        [TestMethod]
+        public void DatabaseFactoryTest() {
+
+        }
+    }
+}
